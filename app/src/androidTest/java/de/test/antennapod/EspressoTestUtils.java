022a5193c7643579dad6ee9f611d23b21eeaa421
Michael Davison <mpdavison@github.com>
2026-07-22 14:47:02 -0700

    Refactor clickPreference method to simplify descendant matching

--- app/src/androidTest/java/de/test/antennapod/EspressoTestUtils.java
+++ app/src/androidTest/java/de/test/antennapod/EspressoTestUtils.java
@@ -212,8 +212,7 @@ public class EspressoTestUtils {
     public static void clickPreference(@StringRes int title) {
         onView(withId(R.id.recycler_view)).perform(
                 RecyclerViewActions.actionOnItem(
-                        allOf(hasDescendant(withText(title)),
-                                hasDescendant(withId(android.R.id.widget_frame))),
+                        hasDescendant(withId(android.R.id.widget_frame)),
                         click()));
     }
 
